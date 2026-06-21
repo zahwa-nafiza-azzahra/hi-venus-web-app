6d2443972880a2456c5046d15e24e5da7a546241
Fahri563 <fahriirfandi564@gmail.com>
2026-06-21 21:29:03 +0700

    Fix Warna pilihan warna yang tidak terlihat diuser

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -12,4 +12,29 @@ class ProductVariant extends Model
     {
         return $this->belongsTo(Product::class);
     }
+
+    public function getColorHexAttribute($value)
+    {
+        if (!empty($value)) {
+            return $value;
+        }
+
+        $colorMap = [
+            'pink' => '#ff85d0',
+            'blue' => '#38bbef',
+            'yellow' => '#fdd73b',
+            'white' => '#ffffff',
+            'black' => '#000000',
+            'red' => '#ef4444',
+            'green' => '#22c55e',
+            'purple' => '#a855f7',
+            'orange' => '#f97316',
+            'brown' => '#a16207',
+            'grey' => '#6b7280',
+            'gray' => '#6b7280',
+        ];
+
+        $colorKey = strtolower($this->attributes['color'] ?? '');
+        return $colorMap[$colorKey] ?? null;
+    }
 }
